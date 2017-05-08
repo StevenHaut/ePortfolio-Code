054eaca1090c30b3df6d3bd208e3a43880a0ca82
StevenHaut <shaut@students.mchenry.edu>
2017-05-07 19:57:40 -0500

    Delete Pennies for Pay.py

--- Pennies for Pay.py
+++
@@ -1,8 +0,0 @@
-number_of_days = int(input("Enter the number of days: "))
-salary = 0
-pennies = 0.01
-
-for days in range(1, number_of_days + 1):
-    salary += pennies
-    pennies *= 2
-    print("Your salary on day", days, "is $", format(salary, ',.2f'))
